9872101203cef075ea77678996b6f62c3da0ba0d
Aashutosh Dutta <aashutosh.x.dutta@gsk.com>
2018-12-02 20:27:25 +0000

    uncommented the API call.

--- PoC_WebApp/Controllers/HomeController.cs
+++ PoC_WebApp/Controllers/HomeController.cs
@@ -39,8 +39,8 @@ namespace PoC_WebApp.Controllers
         {
             ViewData["Message"] = "Please find the API response below";
 
-            //string URL = "http://172.26.51.204/api/values";
-           //  string URL = "http://localhost:54271/api/values";
+            string URL = "http://172.26.51.204/api/values";
+           //string URL = "http://localhost:54271/api/values";
             string result = "";
 
             using (var httpClient = new HttpClient())
